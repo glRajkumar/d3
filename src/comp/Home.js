1602b79a311d6a0ab51c11f45e59a04d7f934b77
Raj kumar <glrajv369@gmail.com>
2022-04-22 16:50:59 +0530

    initialised emtpy repo

---
+++ src/comp/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='h-screen flex items-center justify-center text-6xl bg-slate-900 text-white'>
+      Hi, welcome!!!
+    </div>
+  )
+}
+
+export default Home
