ddac403c800a6b09c43d15e74dfeb493d9da3bfc
Raj kumar <glrajv369@gmail.com>
2022-05-02 10:30:10 +0530

    axes added to list0 - exm1

--- src/comp/Example1/List0.js
+++ src/comp/Example1/List0.js
@@ -25,18 +25,22 @@ const data = [
 ]
 
 function BarChart() {
+  let toMargin = 20
+
   useEffect(() => {
+    let elDimentions = document.getElementById("exmpe1-list0-svg0").getBoundingClientRect()
     let max = d3.max(data, d => d.age)
+
     let widthScale = d3.scaleLinear()
-      .range([0, 384])
+      .range([0, elDimentions.width - toMargin])
       .domain([0, max])
 
     let positionScale = d3.scaleBand()
-      .range([0, 384])
+      .range([0, elDimentions.height - toMargin])
       .domain(data.map(d => d.age))
       .padding(0.1)
 
-    d3.select("#exmpe1-list0")
+    d3.select("#exmpe1-list0-rects")
       .selectAll("rect")
       .data(data)
       .enter()
@@ -46,14 +50,25 @@ function BarChart() {
       .attr('height', positionScale.bandwidth())
       .attr('y', d => positionScale(d.age))
 
+    let xAxis = d3.axisBottom(widthScale)
+    d3.select("#exmpe1-list0-xAxis")
+      .style('transform', `translate(${toMargin}px, ${elDimentions.height - toMargin}px)`)
+      .call(xAxis)
+
+    let yAxis = d3.axisLeft(positionScale)
+    d3.select("#exmpe1-list0-yAxis")
+      .style('transform', `translateX(${toMargin}px)`)
+      .call(yAxis)
+
+    // eslint-disable-next-line
   }, [])
 
   return (
     <div>
-      <svg
-        id='exmpe1-list0'
-        className="w-96 h-96 border border-black"
-      >
+      <svg id='exmpe1-list0-svg0' className="w-96 h-96 border border-black">
+        <g id='exmpe1-list0-rects' style={{ transform: `translateX(${toMargin}px)` }}></g>
+        <g id='exmpe1-list0-xAxis'></g>
+        <g id='exmpe1-list0-yAxis'></g>
       </svg>
     </div>
   )
