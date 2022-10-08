cfdfb4a4c0d9debe86de4be064e51dad3e200ef1
Raj kumar <glrajv369@gmail.com>
2022-10-08 17:52:34 +0530

    navigaton added

--- src/comp/Home.js
+++ src/comp/Home.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import { Link } from 'react-router-dom';
+
+const links = [
+  {
+    path: "/example0",
+    title: "Example0"
+  },
+  {
+    path: "/example1",
+    title: "Example1"
+  },
+]
 
 function Home() {
   return (
-    <div className='h-screen flex items-center justify-center text-6xl bg-slate-900 text-white'>
-      Hi, welcome!!!
+    <div className='h-screen p-4 text-slate-900 overflow-y-auto'>
+      <ul>
+        {
+          links.map(li => (
+            <li key={li.title} className='my-2'>
+              <Link className='hover:underline' to={li.path}>
+                {li.title}
+              </Link>
+            </li>
+          ))
+        }
+      </ul>
     </div>
   )
 }
