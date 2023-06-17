a9a853be7983475a0d09c1ddc396321a5d2f823c
Raj kumar <glrajv369@gmail.com>
2023-06-17 17:50:37 +0530

    map with marks added

--- src/comp/Example1/List3.js
+++ src/comp/Example1/List3.js
@@ -2,12 +2,14 @@ import { useEffect } from 'react';
 import * as d3 from 'd3';
 
 import countriesGeoData from '../../dataStore/countries.geo.json'
+import countriesData2 from '../../dataStore/country2.csv'
 import countriesData from '../../dataStore/country.csv'
 
 function List3() {
   useEffect(() => {
     drawWorldMap()
     drawChoroplethMap()
+    drawWorldMapWithMarks()
     // eslint-disable-next-line
   }, [])
 
@@ -75,12 +77,50 @@ function List3() {
       .attr("fill", d => d.properties.Magnitude ? cScale(d.properties.Magnitude) : "white")
   }
 
+  async function drawWorldMapWithMarks() {
+    let bodyHeight = 400
+    let bodyWidth = 400
+
+    const data = await d3.csv(countriesData2)
+
+    let projection = d3.geoMercator()
+      .scale(50)
+      .translate([bodyWidth / 2, bodyHeight / 2])
+
+    let path = d3.geoPath()
+      .projection(projection)
+
+    d3.select("#exmpe1-list3-svg2")
+      .selectAll("path")
+      .data(countriesGeoData.features)
+      .enter()
+      .append("path")
+      .attr('d', d => path(d))
+      .attr("stroke", "#999")
+      .attr("fill", "#eee")
+
+    d3.select("#exmpe1-list3-svg2")
+      .selectAll("circle")
+      .data(data)
+      .enter()
+      .append("circle")
+      .attr("r", 1)
+      .attr("fill", "#0055AA")
+      .style("opacity", 0.5)
+      .attr("cx", d => projection([+d.Longitude, +d.Latitude])[0])
+      .attr("cy", d => projection([+d.Longitude, +d.Latitude])[1])
+
+  }
+
   return (
     <div className='flex flex-wrap gap-6'>
       <svg id='exmpe1-list3-svg0' className="w-[400px] h-[400px] border border-slate-300">
       </svg>
 
       <svg id='exmpe1-list3-svg1' className="w-[400px] h-[400px] border border-slate-300">
+      </svg>
+
+      <svg id='exmpe1-list3-svg2' className="w-[400px] h-[400px] border border-slate-300">
       </svg>
     </div>
   )
